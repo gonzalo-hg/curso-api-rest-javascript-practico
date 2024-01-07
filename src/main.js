998ccd22f19c62569b65f7704427c3c211a3f187
gonzalohg <gonzalo.hg19@gmail.com>
2024-01-07 15:14:05 -0600

    Clase 06: Lista de categorias

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-async function getTrendingMoviesPreview(){
+async function getTrendingMoviesPreview() {
     const response = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
     const data = await response.json();
 
@@ -11,13 +11,37 @@ async function getTrendingMoviesPreview(){
 
         const movieImg = document.createElement('img');
         movieImg.classList.add('movie-img');
-        movieImg.setAttribute('alt',movie.title);
+        movieImg.setAttribute('alt', movie.title);
         movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + movie.poster_path);
 
         movieContainer.appendChild(movieImg);
         trendingPreviewMoviesContainer.appendChild(movieContainer);
     });
-    
+
+}
+
+async function getCategoriesPreview() {
+    const response = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await response.json();
+
+    const categories = data.genres;
+    categories.forEach(category => {
+        const previewCategoryContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id','id' + category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoryContainer.appendChild(categoryContainer);
+    });
+
 }
 
 getTrendingMoviesPreview();
+getCategoriesPreview();
